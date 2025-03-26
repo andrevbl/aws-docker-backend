43bbb5a5a0d3e444bf06bfe74f9a937adca88500
Andre Valente <70276822+andrevbl@users.noreply.github.com>
2025-03-26 16:57:21 -0300

    Create Program.cs

---
+++ src/services/MyProject/Program.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace Cofin.Diligencia.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development)
+            {
+                Console.WriteLine("Log: Creating Development HostBuilder");
+
+                return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseIISIntegration();
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
+            }
+            else
+            {
+                Console.WriteLine("Log: Creating non-Development HostBuilder");
+
+                return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel(options =>
+                    {
+                        options.Listen(IPAddress.IPv6Any, 443, listenOptions =>
+                        {
+                            listenOptions.UseHttps(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Certificates/docker.myproject.pfx"), "");
+                        });
+                        options.Listen(IPAddress.IPv6Any, 80);
+                        options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
+                    });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
+            }
+        }
+    }
+}
