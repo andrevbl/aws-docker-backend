2d70536c16dec859b6a4e21b95ddf67ce47416f6
Andre Valente <70276822+andrevbl@users.noreply.github.com>
2025-03-26 17:02:10 -0300

    Update Program.cs

--- src/services/MyProject/Program.cs
+++ src/services/MyProject/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
 
-namespace Cofin.Diligencia.API
+namespace MyProject.API
 {
     public class Program
     {
